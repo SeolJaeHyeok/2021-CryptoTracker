07014f109902cb097db1cb5594d0083c9a3193d1
Jaehyeok <milkboy2564@naver.com>
2021-12-19 07:32:03 +0900

    HashRouter로 변경
    1) BrowserRouter를 사용하니까 계속 README 파일이 보여지길래 구글링을 통해 발견한 방법 적용

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,17 +1,17 @@
-import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { HashRouter, Route } from "react-router-dom";
 import Coin from "./routes/Coin";
 import Coins from "./routes/Coins";
 
 function Router() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Route path="/:coinId">
         <Coin />
       </Route>
       <Route exact path="/">
         <Coins />
       </Route>
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 export default Router;
